378e37a503d343bfdf64d23b7e8d309b6052a9d6
Max Rittmüller <max.rittmueller@gmail.com>
2016-01-23 10:47:26 +0100

    Add Babel stuff to tests

--- test/index.js
+++ test/index.js
@@ -11,42 +11,20 @@ var assert = require('assert'),
 
 describe('Arguments', function() {
   it('should not accept too few arguments', function() {
-    expect(function() {
-      autostart.enableAutostart('someString', 'anotherString', 'stillAString');
-    }).to.throw(Error);
-    expect(function() {
-      autostart.disableAutostart('someString');
-    }).to.throw(Error);
-    expect(function() {
-      autostart.isAutostartEnabled('someString');
-    }).to.throw(Error);
+    expect(() => autostart.enableAutostart('someString', 'anotherString', 'stillAString')).to.throw(Error);
+    expect(() => autostart.disableAutostart('someString')).to.throw(Error);
+    expect(() => autostart.isAutostartEnabled('someString')).to.throw(Error);
   });
 
   it('should not accept arguments with wrong types', function() {
-    expect(function() {
-      autostart.enableAutostart(1, 2, 3, 4);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.enableAutostart('string', 2, 3, 4);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.enableAutostart('string', 'string', 3, 4);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.enableAutostart('string', 'string', 'string', 4);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.disableAutostart(1, 2);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.disableAutostart('string', 2);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.isAutostartEnabled(1, 2);
-    }).to.throw(Error);
-    expect(function() {
-      autostart.isAutostartEnabled('string', 2);
-    }).to.throw(Error);
+    expect(() => autostart.enableAutostart(1, 2, 3, 4)).to.throw(Error);
+    expect(() => autostart.enableAutostart('string', 2, 3, 4)).to.throw(Error);
+    expect(() => autostart.enableAutostart('string', 'string', 3, 4)).to.throw(Error);
+    expect(() => autostart.enableAutostart('string', 'string', 'string', 4)).to.throw(Error);
+    expect(() => autostart.disableAutostart(1, 2)).to.throw(Error);
+    expect(() => autostart.disableAutostart('string', 2)).to.throw(Error);
+    expect(() => autostart.isAutostartEnabled(1, 2)).to.throw(Error);
+    expect(() => autostart.isAutostartEnabled('string', 2)).to.throw(Error);
   });
 });
 
